1c86fed0ef43972383ba344c3fb5c72a32d5900f
keen91 <kinan.hatahet@gmail.com>
2022-06-21 13:23:20 +0300

    Added react query

--- src/App.js
+++ src/App.js
@@ -3,23 +3,45 @@ import { useEffect } from "react";
 import "./style.css";
 import { useState } from "react";
 import SingleProduct from "./components/SingleProductCard";
+import { getProducts } from "./util/fetch";
 
+import { Link } from "react-router-dom";
+
+// React query import
+import { useQuery } from "react-query";
 
 export default function App() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch ("https://fakestoreapi.com/products")
-    .then(response => response.json())
-    .then(json => setData(json))
-    .then(console.log(data));
-  }, [])
+  // const [data, setData] = useState([]);
+  // useEffect(() => {
+  //   fetch ("https://fakestoreapi.com/products")
+  //   .then(response => response.json())
+  //   .then(json => setData(json))
+  //   .then(console.log(data));
+  // }, [])
+
+  const { isLoading, data, error } = useQuery("products", getProducts);
+
+  if (isLoading) {
+    return <div className="ourTeam">Loading...</div>;
+  }
+
+  if (error) {
+    return <div className="ourTeam">Something went wrong... try again</div>;
+  }
 
   return (
-      <main className="allProducts" style={{padding:"30px"}}>
-        <h2>Products</h2>
-        <section className="productCards">
-          <SingleProduct data={data} />
-        </section>
-      </main>
+    <main className="allProducts" style={{ padding: "30px" }}>
+      <h2>Products</h2>
+
+      <section className="productCards">
+        {data.map((product) => {
+          return (
+            <Link to={`/product/${product.id}`} key={product.id}>
+              <SingleProduct product={product} />
+            </Link>
+          );
+        })}
+      </section>
+    </main>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,31 +1,43 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import Navbar from "./components/Navbar";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link,
-  Routes
+  Routes,
 } from "react-router-dom";
 
-import App from './App';
-import About from './components/About';
-import SingleProductPage from './components/SingleProductPage';
+import App from "./App";
+import About from "./routes/About";
+import SingleProductPage from "./routes/SingleProductPage";
+import { ReactQueryDevtools } from 'react-query/devtools';
 
-const rootElement = document.getElementById('root');
+
+// React query import
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
+
+// Create a client
+const queryClient = new QueryClient();
 
 root.render(
   <StrictMode>
-    <Navbar/>
+    <QueryClientProvider client={queryClient}>
+      <Navbar />
       <Router>
         <Routes>
-          <Route exact path="/" element={<App/>} />
-          <Route exact path="/about" element={<About/>} />
+          <Route exact path="/" element={<App />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/product/:productId" element={<SingleProductPage />} />
           {/* <Route exact path="product/:id" element={<SingleProductPage/>} /> */}
           {/* I added the route above but i didnt continue to work */}
         </Routes>
       </Router>
-    </StrictMode>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </StrictMode>
 );
